eb4ee97801b4653a221cf6100ea8a72c3e8e32e2
Gael Pasgrimaud <gael@gawel.org>
2013-01-28 22:13:58 +0100

    docsting

--- chut/examples/sudo.py
+++ chut/examples/sudo.py
@@ -4,5 +4,9 @@ from chut import * # noqa
 
 @console_script
 def safe_upgrade(args):
+    """Usage: %prog
+
+    Update && upgrade a debian based system
+    """
     sudo.aptitude('update') > 1
-    sudo.aptitude('safe-upgrade -y') > 1
+    sudo.aptitude('safe-upgrade') > 1
